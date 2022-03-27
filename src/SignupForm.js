1ee612f90d76061486df3e012f09cd3d8df4aa69
krishnaveni44 <krishnaveni44@gmail.com>
2022-03-27 16:30:02 +0530

    css

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import { useHistory } from "react-router-dom";
+import TextField from '@mui/material/TextField';
+import * as yup from "yup";
+
+export const userdetailValidationSchema = yup.object({
+
+  Image: yup.string().required("Why not fill this Image? 😀"),
+  FirstName: yup.string().required("Why not fill this FirstName? 😀"),
+  LastName: yup
+      .string()
+      .required("Why not fill this LastName? 😀")
+      .min(4, "Need a longer LastName 😀"),
+  Email: yup.string().required("Why not fill this  Email? 😀"),
+  Password: yup
+  .string()
+  .required("Why not fill this Password 😀")
+  .min(8, "Need a longer Password 😀"),
+  TypeOfuser: yup
+  .string()
+  .required("Why not fill this TypeOfuser? 😀")
+  .min(4, "Need a longer TypeOfuser 😀"),
+});
+
 
 export const SignupForm = () => {
   const history = useHistory();
@@ -12,6 +34,7 @@ export const SignupForm = () => {
       Password: '',
       TypeOfuser: '',
     },
+    validationSchema: userdetailValidationSchema,
     onSubmit: values => {
 
       fetch(`http://localhost:4000/userdetails`, {
@@ -27,23 +50,66 @@ export const SignupForm = () => {
     },
   });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
+    <form onSubmit={formik.handleSubmit} className="add-userdetail-form">
+     
+     <TextField
+        label="FirstName"
+        name="FirstName"
+        id="name"        
+        type="text"
+        variant="outlined"
+        onChange={formik.handleChange}
+        value={formik.values.FirstName}
+        onBlur = {formik.handleBlur}
+        error = {formik.touched.FirstName && formik.errors.FirstName}
+        helperText = {formik.touched.FirstName && formik.errors.FirstName ? formik.errors.FirstName : ""}
+         // onChange={(event) => setName(event.target.value)}    
+        />
+         <TextField
+        type = "text"
+        label="LastName"
+        id = "name"
+        name="LastName"
+        variant="outlined"
+        // placeholder = "Poster"
+        onChange={formik.handleChange}
+        value={formik.values.LastName}
+        onBlur = {formik.handleBlur}
+        error = {formik.touched.LastName && formik.errors.LastName}
+        helperText = {
+          formik.touched.LastName && formik.errors.LastName ? formik.errors.LastName : ""
+        }
+         />
+           {/* <TextField
+        type ="text"
+        label="Email"
+        id="email"
+        name="Email"
+        variant="outlined" 
+        //placeholder = "Rating" 
+        onChange={formik.handleChange}
+        value={formik.values.Email}
+        onBlur = {formik.handleBlur}
+        error = {formik.touched.Email && formik.errors.Email}
+        helperText = {formik.touched.Email && formik.errors.Email ? formik.errors.Email : ""}
+         /> */}
+      {/* <label htmlFor="firstName">First Name</label>
       <input
         id="firstName"
         name="FirstName"
         type="text"
         onChange={formik.handleChange}
         value={formik.values.FirstName}
-      />
-      <label htmlFor="lastName">Last Name</label>
+      /> */}
+      {/* <label htmlFor="lastName">Last Name</label>
       <input
         id="lastName"
         name="LastName"
         type="text"
         onChange={formik.handleChange}
         value={formik.values.LastName}
-      />
+      /> */}
+      <div className='field-row'>
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
@@ -52,6 +118,8 @@ export const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.Email}
       />
+      </div>
+      <div className='field-row'>
        <label htmlFor="password">Password</label>
       <input
         id="password"
@@ -60,6 +128,14 @@ export const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.Password}
       />
+      </div>
+       <div className='field-row'>
+      <select id="TypeOfuser" name="TyprOfuser">
+          <option value="Admin">Admin</option>
+          <option value="Manager">Manager</option>
+          <option value="Employee">Employee</option>
+        </select>
+        </div>
        {/* <label htmlFor="TypeOfuser">TypeOfuser</label>
       <input
         id="TypeOfuser"
